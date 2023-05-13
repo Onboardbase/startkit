53ed80b5353d58b014452f06d72bc7644f079ac5
olusamimaths <olusola.samuel.oluwatobi@gmail.com>
2023-05-13 16:13:00 +0100

    ⚡️ WIP

---
+++ cmd/root.go
@@ -0,0 +1,34 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "obbkit",
+	Short: "CLI tool for generating boilerplates projects with Onboardbase completely setup.",
+	Aliases: []string{"obb"},
+	Version: "0.0.1",
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	versionTemplate := `{{printf "%s: %s - version %s\n" .Name .Short .Version}}`
+	rootCmd.SetVersionTemplate(versionTemplate)
+}
+
+
