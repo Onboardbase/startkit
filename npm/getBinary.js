3276e480f322b5006e97b095bf1e92d5f059f894
olusamimaths <olusola.samuel.oluwatobi@gmail.com>
2023-06-20 22:29:52 +0100

    🔖 v1.0.0

--- npm/getBinary.js
+++ npm/getBinary.js
@@ -1,26 +1,53 @@
 const { Binary } = require("binary-install");
 const os = require("os");
 
-function getPlatform() {
+function getSystemInfo() {
   const type = os.type();
   const arch = os.arch();
 
-  if (type === "Windows_NT" && arch === "x64") return "win64";
-  if (type === "Windows_NT") return "win32";
-  if (type === "Linux" && arch === "x64") return "linux";
-  if (type === "Darwin" && arch === "x64") return "macos";
+  if (type === "Windows_NT" && arch === "x64") {
+    return {
+      platform: "win32",
+      arch: "x64",
+    };
+  }
+  if (type === "Windows_NT") {
+    return {
+      platform: "win32",
+      arch: "x86",
+    };
+  }
+  if (type === "Linux" && arch === "x64") {
+    return {
+      platform: "linux",
+      arch: "x64",
+    };
+  }
+  if (type === "Darwin" && arch === "x64") {
+    return {
+      platform: "Darwin",
+      arch: "x64",
+    };
+  }
+  if (type === "Darwin" && arch === "arm64") {
+    return {
+      platform: "Darwin",
+      arch: "arm64",
+    };
+  }
 
   throw new Error(`Unsupported platform: ${type} ${arch}`);
 }
 
 function getBinary() {
-  const platform = getPlatform();
+  const { platform, arch } = getSystemInfo();
   const version = require("../package.json").version;
   //   https://github.com/Onboardbase/onboardbasekit/releases/download/v0.0.7/onboardbasekit_Darwin_x86_64.tar.gz
-  const url = `https://github.com/Onboardbase/onboardbasekit/releases/download/v${version}/onboardbasekit_${platform}.tar.gz`;
+  const url = `https://github.com/Onboardbase/onboardbasekit/releases/download/v${version}/onboardbasekit_${platform}_${arch}.tar.gz`;
   //   url: "https://github.com/Onboardbase/onboardbasekit/releases/download/v{{version}}/onboardbasekit_{{platform}}_{{arch}}.tar.gz",
   const name = "onboardbasekit";
-  return new Binary(url, { name });
+  console.log("url", url);
+  return new Binary(name, url);
 }
 
 module.exports = getBinary;
