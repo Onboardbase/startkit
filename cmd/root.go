5003597b689cd1f763be9ed73fa622d6d5fbde48
olusamimaths <olusola.samuel.oluwatobi@gmail.com>
2023-06-21 13:14:57 +0100

    🔖 v1.0.3

--- cmd/root.go
+++ cmd/root.go
@@ -14,7 +14,7 @@ var rootCmd = &cobra.Command{
 	Use:     "onboardbasekit",
 	Short:   "CLI tool for generating boilerplates projects with Onboardbase completely setup.",
 	Aliases: []string{"obb"},
-	Version: "1.0.2",
+	Version: "1.0.3",
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
